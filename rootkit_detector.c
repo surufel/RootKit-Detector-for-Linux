d90311ae804afd46ca33910f1bc83d7ecaa82a4d
surufel <henriquefrancasmm@gmail.com>
2026-01-29 17:51:23 -0300

    fixes: refactor scanner logic to validate TGIDs and process persistence to remove false-positive

--- rootkit_detector.c
+++ rootkit_detector.c
@@ -8,6 +8,7 @@
 #include <ctype.h>
 #include <stdint.h>
 #include "rk_scanning.h"
+#include <string.h>
 
 typedef struct {
     int pid;
@@ -15,24 +16,47 @@ typedef struct {
     bool is_hidden;
 }suspectProcess;
 
-void deep_scan_signals(){
+void deep_scan_signals() {
     printf("[*] Starting deep scan for hidden processes...\n");
     int hidden_count = 0;
 
     for(int pid = 1; pid < MAX_PID; pid++){
-        // Syscall for Kernel
-        if(kill(pid, 0) == 0 || errno == EPERM){
-            if (!is_marked(pid)){
-                // Deals with false-positives
-                usleep(1000);
-                if(kill(pid, 0) == 0 || errno == EPERM){
-                    printf("[!] ALERT: Hidden PID detected: %d\n", pid);
-                    hidden_count++;
-                }
+        // Verifies if the PID exists
+        if (kill(pid, 0) == 0 || errno == EPERM){
+            	if(!is_marked(pid)){ // If Kernel confirms it exists in /procs, but it didn't got marked
+                // Tries to read the process's name
+                char path[64], name[64];
+                snprintf(path, sizeof(path), "/proc/%d/comm", pid);
+                FILE *f = fopen(path, "r");
+		if(f){
+			if(fgets(name, sizeof(name), f)){
+				name[strcspn(name, "\n")] = 0;
+				// Checks out if it is a thread reading /proc/[pid]/status
+				char status_path[64], line[128];
+				int tgid = 0; // TGID = Thread Group ID
+				snprintf(status_path, sizeof(status_path), "/proc/%d/status", pid);
+				FILE *fs = fopen(status_path, "r"); // fs = file stream
+				if (fs){
+					while(fgets(line, sizeof(line), fs)) {
+						if(strncmp(line, "Tgid:", 5) == 0){
+							tgid = atoi(line + 6);
+							break;
+						}
+					}
+					fclose(fs);
+				}
+				// If PID !=  TGID, it means that it is a thread.
+				if(tgid == pid){
+					printf("[!] ALERT: REAL Hidden Process detected: %d [%s]\n", pid, name);
+					hidden_count++;
+				}
+			}
+			fclose(f);
+		}
             }
         }
     }
-    printf("[+] Scan finished. Suspects found: %d\n", hidden_count);
+printf("[+] Scan finished. Suspects found: %d\n", hidden_count);
 }
 
 int main(){
