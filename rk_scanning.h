2bb13ce5892c2c7432a8eb7ff0815b35af7c4918
surufel <henriquefrancasmm@gmail.com>
2026-01-29 19:11:54 -0300

    fix and add: added is_pid_dir method for a fix in scan_proc_dir

--- rk_scanning.h
+++ rk_scanning.h
@@ -26,6 +26,15 @@ static bool is_marked(int pid){
      return proc_pids[pid / 8] & (1 << (pid % 8)); // Expected result is either 0 or x higher than 0. It grabs where is the processed PID, and bit-shifts 1 for (pid%8) times, and uses the &(AND) operator with the PID.
 }
 
+// Checks if the directory is numeric (a PID)
+int is_pid_dir(const char *name) {
+    while (*name) {
+        if (*name < '0' || *name > '9') return 0;
+        name++;
+    }
+    return 1;
+}
+
 static void scan_proc_dir() {
     DIR *dir = opendir("/proc");
     struct dirent *entry;
@@ -33,7 +42,7 @@ static void scan_proc_dir() {
     // Iterates over each directory entry
     while ((entry = readdir(dir)) != NULL) {
         // The program will look for PIDs. d_type == DT_DIR verifies if the file is a directory, and checks out if it is a digit as well (because PIDs are directories named as numbers)
-        if(entry->d_type == DT_DIR && isdigit(entry->d_name){
+        if(entry->d_type == DT_DIR && is_pid_dir(entry->d_name)){
             int pid = atoi(entry->d_name);
             char path[64];
             snprintf(path, sizeof(path), "/proc/%d/comm", pid);
