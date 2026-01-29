8e91579ac117291b51d547bcecbfed271c1ca0fd
surufel <henriquefrancasmm@gmail.com>
2026-01-29 19:23:34 -0300

    fix: overhaul in the scanning method

--- rootkit_detector.c
+++ rootkit_detector.c
@@ -24,38 +24,32 @@ void deep_scan_signals() {
         // Verifies if the PID exists
         if (kill(pid, 0) == 0 || errno == EPERM){
             	if(!is_marked(pid)){ // If Kernel confirms it exists in /procs, but it didn't got marked
-                // Tries to read the process's name
-                char path[64], name[64];
-                snprintf(path, sizeof(path), "/proc/%d/comm", pid);
-                FILE *f = fopen(path, "r");
-		if(f){
-			if(fgets(name, sizeof(name), f)){
-				name[strcspn(name, "\n")] = 0;
-				// Checks out if it is a thread reading /proc/[pid]/status
-				char status_path[64], line[128];
-				int tgid = 0; // TGID = Thread Group ID
-				snprintf(status_path, sizeof(status_path), "/proc/%d/status", pid);
-				FILE *fs = fopen(status_path, "r"); // fs = file stream
-				if (fs){
-					while(fgets(line, sizeof(line), fs)) {
-						if(strncmp(line, "Tgid:", 5) == 0){
+				char path[64];
+				snprintf(path, sizeof(path), "/proc/%d/status", pid);
+				FILE *f = fopen(path, "r");
+		if(f == NULL){
+			printf("[!] ALERT: HIDDEN PID DETECTED (Inaccessible /proc): %d\n", pid);
+			hidden_count++;
+		} else {
+			// If the file opens, we'll check if it is a TGID
+			int tgid = -1;
+			char line[128];
+			while (fgets(line, sizeof(line), f)){
+				if (strncmp(line, "Tgid:", 5) == 0){
 							tgid = atoi(line + 6);
 							break;
 						}
 					}
-					fclose(fs);
+					fclose(f);
+
+					if (tgid == pid) {
+						printf("[!] ALERT: HIDDEN PROCESS DETECTED: %d\n", pid);
+						hidden_count++;
+					}
 				}
-				// If PID !=  TGID, it means that it is a thread.
-				if(tgid == pid){
-					printf("[!] ALERT: REAL Hidden Process detected: %d [%s]\n", pid, name);
-					hidden_count++;
-				}
-			}
-			fclose(f);
 		}
-            }
-        }
-    }
+	}
+}
 printf("[+] Scan finished. Suspects found: %d\n", hidden_count);
 }
 
