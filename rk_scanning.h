8e24134243120ea74b6879dbf3693c625ce3ef82
surufel <henriquefrancasmm@gmail.com>
2026-01-30 00:18:25 -0300

    changes: fopen to struct status, switched comm dir -> status. Added log prints

--- rk_scanning.h
+++ rk_scanning.h
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
 #define MAX_PID 32768 // 4KB
 #define BITSET_SIZE (MAX_PID / 8) // Gets the max size of PIDs and divides by 1 byte, managing the PIDs and grabbing only their index.
@@ -35,25 +36,26 @@ int is_pid_dir(const char *name) {
     return 1;
 }
 
-static void scan_proc_dir() {
+static void scan_proc_dir(){
     DIR *dir = opendir("/proc");
     struct dirent *entry;
     if (!dir){return;}
     // Iterates over each directory entry
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL){
+	    printf("[LOG] Found entry: %s\n", entry->d_name);
         // The program will look for PIDs. d_type == DT_DIR verifies if the file is a directory, and checks out if it is a digit as well (because PIDs are directories named as numbers)
         if(entry->d_type == DT_DIR && is_pid_dir(entry->d_name)){
             int pid = atoi(entry->d_name);
             char path[64];
-            snprintf(path, sizeof(path), "/proc/%d/comm", pid);
-
-            if(access(path, F_OK) == 0){
-                mark_pid(pid);
-            } // Marks the PID.
+            snprintf(path, sizeof(path), "/proc/%d/status", pid);
+	    struct stat st;
+	    if(stat(path, &st) == 0){
+		    mark_pid(pid);
          }
      }
-     closedir(dir);
-     printf("[+] /proc scanning completed.\n");
- }
+    }
+    closedir(dir);
+    printf("[+] /proc scanning completed.\n");
+}
 
 #endif
